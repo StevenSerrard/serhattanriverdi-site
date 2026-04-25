22396058e23e8e638cdf8329ee3663a566d03fcb
StevenSerrard <91534648+StevenSerrard@users.noreply.github.com>
2026-04-25 22:16:35 +0300

    Increase intersection observer threshold to 0.25

--- script.js
+++ script.js
@@ -18,10 +18,12 @@ const revealObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add('is-visible');
+    } else {
+      entry.target.classList.remove('is-visible');
     }
   });
 }, {
-  threshold: 0.2
+  threshold: 0.25
 });
 
 revealElements.forEach((element) => {
